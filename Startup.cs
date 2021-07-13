62afe510575e12ca13e998bd0b69ee27f9e108ee
Erinxon <30541728+Erinxon@users.noreply.github.com>
2021-07-13 18:40:46 -0400

    AddCors

--- Startup.cs
+++ Startup.cs
@@ -27,8 +27,11 @@ namespace ApiCombustibles
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {
-
+        {       
+            services.AddCors(options => options.AddPolicy("ApiCorsPolicy", builder =>
+            {
+                builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+            }));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -43,6 +46,11 @@ namespace ApiCombustibles
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(builder => builder
+                   .AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader());
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
